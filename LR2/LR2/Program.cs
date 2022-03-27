bdb0df8382bc729e92c35be8bbad43dd0660dde0
TolaFish <joker270819955@gmail.com>
2022-03-27 15:25:33 +0500

    new master

---
+++ LR2/LR2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write A");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write B");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Режимы: Сложение +, вычитание -, умножить *, разделить /");
+            string X = Convert.ToString(Console.ReadLine());
+            string Plus;
+            string Minus;
+            string Umn;
+            string Razd;
+
+
+
+
+
+        }
+    }
+}
